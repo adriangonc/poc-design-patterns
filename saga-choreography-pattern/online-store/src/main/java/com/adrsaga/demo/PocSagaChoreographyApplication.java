f77ec494f0c10ec4fe5f58d4f7dea42465fafdd7
Adriano Goncalves <tqi_agoncalves@uolinc.com>
2023-03-27 18:09:13 -0300

    Criação de poc de microserviços utilizando o pattern saga

---
+++ saga-choreography-pattern/online-store/src/main/java/com/adrsaga/demo/PocSagaChoreographyApplication.java
@@ -0,0 +1,13 @@
+package com.adrsaga.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocSagaChoreographyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocSagaChoreographyApplication.class, args);
+	}
+
+}
---
+++ saga-choreography-pattern/online-store/src/test/java/com/adrsaga/demo/PocSagaChoreographyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adrsaga.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocSagaChoreographyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
